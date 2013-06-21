6c0a8e832e2c72c99b301098a0c408d464a12bba
Nick Crawford <crawfonw@rose-hulman.edu>
2013-06-20 22:51:05 -0400

    First files; lots of empty things!

---
+++ pychess/boards.py
@@ -0,0 +1,11 @@
+'''
+Created on Jun 20, 2013
+
+@author: nick
+'''
+
+class Board():
+    
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height    
---
+++ pychess/gui.py
@@ -0,0 +1,8 @@
+'''
+Created on Jun 20, 2013
+
+@author: nick
+'''
+
+if __name__ == '__main__':
+    pass
